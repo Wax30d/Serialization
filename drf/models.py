61500fe581c7fd2ed5f470aebde15729f5825b89
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-01 20:36:11 +0500

    Created Merchant model.

--- drf/models.py
+++ drf/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Merchant(models.Model):
+    merchant_name = models.CharField(max_length=250)
+    merchant_code = models.CharField(max_length=250)
+    merchant_address = models.TextField()
