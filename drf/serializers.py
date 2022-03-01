d27f560360e1007ec6364b4bb77c6e0165065ca8
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-01 21:19:45 +0500

    Model serializer.

--- drf/serializers.py
+++ drf/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-
+from .models import Merchant
 
 # first style of serialization
 # this class has the ability to serialize fields
-class MerchantSerializer(serializers.Serializer):
-    merchant_name = serializers.CharField()
-    merchant_code = serializers.CharField()
-    merchant_address = serializers.CharField()
+
+# class MerchantSerializer(serializers.Serializer):
+#     merchant_name = serializers.CharField()
+#     merchant_code = serializers.CharField()
+#     merchant_address = serializers.CharField()
+
+
+# second style of serialization is Model serializer
+# this class has the ability to serialize model
+
+class MerchantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Merchant
+        fields = '__all__'
